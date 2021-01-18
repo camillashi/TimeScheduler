451cc13c592e305c10f742d4f6875fe90eab2627
camil <camil@DESKTOP-6FGMN9F.speedport.ip>
2021-01-18 19:56:32 +0100

    First Try!!

---
+++ src/org/xemacscode/demo/DBConnection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.xemacscode.demo;
+import java.sql.*;
+/**
+ *
+ * @author camil
+ */
+public class DBConnection {
+    static final String DB_URL="jdbc:mysql://localhost/mytimescheduler";
+    static final String USER="root";
+    static final String PASS="";
+    public static Connection connectDB()
+    {
+        try{
+            Connection conn=null;
+            //Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection(DB_URL,USER,PASS);
+            return conn;
+        }catch(Exception ex)
+        {
+            System.out.println("There were errors while connecting to db.");
+            return null;
+        }
+    }
+}
